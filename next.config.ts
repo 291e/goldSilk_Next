9d593d4ac60a33a08476620b4dd9f90b5f738656
291e <yko0321@naver.com>
2025-02-03 20:02:19 +0900

    수정

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,8 @@ const isDev = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: isDev ? "" : "/test", // ✅ 배포 환경에서 /test로 설정
-  assetPrefix: isDev ? "" : "/test/", // ✅ 슬래시 중복 방지
   images: {
     domains: ["goldsilk.net"],
-    path: isDev ? "/_next/image" : "/test/_next/image", // ✅ 이미지 경로 수정
   },
 };
 
