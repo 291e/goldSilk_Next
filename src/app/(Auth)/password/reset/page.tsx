f4293e1873115cee171d22694f9625b7920f3edd
291e <yko0321@naver.com>
2025-02-12 13:37:41 +0900

    빌드 에러 수정

--- src/app/(Auth)/password/reset/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/auth/reset-password/page.tsx
-import ResetPasswordForm from "@/features/auth/ui/ResetPasswordForm";
-
-export default function ResetPasswordPage() {
-  return <ResetPasswordForm />;
-}
