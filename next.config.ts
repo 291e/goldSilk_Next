e164f39043b75d20bb5501eea360ae14d6fea5c7
291e <yko0321@naver.com>
2025-02-20 14:30:12 +0900

    베이스 패치 제거

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: isDev ? "" : "/", // ✅ 배포 환경에서 /test로 설정
   images: {
     domains: ["goldsilk.net"],
   },
