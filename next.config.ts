a7ce6990328945d1750468321be9b111d55c0864
291e <yko0321@naver.com>
2025-02-20 14:42:30 +0900

    베이스 패치 문제인가?

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["goldsilk.net"],
   },
+  basePath: "", // ✅ basePath를 비워둠 (불필요한 `/` 제거)
+  trailingSlash: false, // ✅ 자동으로 경로 끝에 `/` 붙지 않게 설정
 };
 
 export default nextConfig;
